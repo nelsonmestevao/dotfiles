9eb597880286da9c5645c3af157bf68d5fbff284
Nelson Estevão <nelsonmestevao@gmail.com>
2018-02-26 22:55:24 +0000

    Update install files for nvim & vim

--- vim/install.sh
+++ vim/install.sh
@@ -2,4 +2,7 @@
 
 sudo apt install vim
 
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
 ln -sfT ~/.dotfiles/vim/vimrc ~/.vimrc
