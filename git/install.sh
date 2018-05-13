c556139b38d26be3895fb57f987982e58e9b6751
Nelson Estevão <nelsonmestevao@gmail.com>
2018-05-13 12:41:12 +0100

    Add git LFS

--- git/install.sh
+++ git/install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-apt install odt2txt
+curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
+
+sudo apt-get install git-lfs
 
 ln -sfT ~/.dotfiles/git/gitconfig ~/.gitconfig
