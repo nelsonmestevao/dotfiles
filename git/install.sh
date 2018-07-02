ab6b4b164c8881a7d74f646315fe0ff0c8c5ad21
Nelson Estevão <nelsonmestevao@gmail.com>
2018-07-02 20:57:49 +0100

    Make scripts quite

--- git/install.sh
+++ git/install.sh
@@ -2,6 +2,7 @@
 
 curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
 
-sudo apt-get install git-lfs
+echo "Installing Git LFS..."
+sudo apt-get install git-lfs -yqq
 
 ln -sfT ~/.dotfiles/git/gitconfig ~/.gitconfig
