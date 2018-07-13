b3b793f53385a524ef6ac65216d6af20494764d2
Nelson Estevão <nelsonmestevao@gmail.com>
2018-07-13 22:46:05 +0100

    Add syntax highlitght for zsh

--- zsh/install.sh
+++ zsh/install.sh
@@ -6,6 +6,7 @@ sudo apt-get install zsh -yqq
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 
 ln -sfT ~/.dotfiles/zsh/zshrc ~/.zshrc
 
