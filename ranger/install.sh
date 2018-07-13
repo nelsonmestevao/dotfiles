a6abf24835604f9877994c974352425e79d2815f
Nelson Estevão <nelsonmestevao@gmail.com>
2018-07-13 12:33:00 +0100

    [ranger]: Add configuration file

--- ranger/install.sh
+++ ranger/install.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
+git clone git@github.com:alexanderjeurissen/ranger_devicons.git
+
 ln -sfT ~/.dotfiles/ranger/rc.conf ~/.config/ranger/rc.conf
+ln -sfT ~/.dotfiles/ranger/rifle.conf ~/.config/ranger/rifle.conf
+
+rm -rf ranger_devicons
