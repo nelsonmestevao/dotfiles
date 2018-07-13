ef69eedcca999c2650cdda04d378542bf2b4a2b5
Nelson Estevão <nelsonmestevao@gmail.com>
2018-07-13 22:53:15 +0100

    Add autosuggestions plugin

--- zsh/install.sh
+++ zsh/install.sh
@@ -7,6 +7,7 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/mas
 
 git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 
 ln -sfT ~/.dotfiles/zsh/zshrc ~/.zshrc
 
