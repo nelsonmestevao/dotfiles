a36ccd60021d7f2ecbd73b2363cf02a27f5683fc
Nelson Estevão <nelsonmestevao@gmail.com>
2018-07-16 00:17:59 +0100

    Remove redshift config

--- redshift/install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "Installing Redshift..."
-sudo apt-get install redshift -yqq
-
-ln -sfT ~/.dotfiles/redshift/redshift.conf ~/.config/redshift.conf
-
--- redshift/uninstall.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-sudo apt-get remove zsh
-
-rm -rf ~/.oh-my-zsh
-
-unlink ~/.zshrc
-
-chsh -s "$(which bash)"
