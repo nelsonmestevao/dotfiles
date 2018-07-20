5fec6194e281fef6cc95dbe8a1d451fdfb9696c2
Nelson Estevão <nelsonmestevao@gmail.com>
2018-07-20 23:43:20 +0100

    Update ranger config

--- ranger/install.sh
+++ ranger/install.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 ln -sfT ~/.dotfiles/ranger/rc.conf ~/.config/ranger/rc.conf
+ln -sfT ~/.dotfiles/ranger/rifle.conf ~/.config/ranger/rifle.conf
--- ranger/uninstall.sh
+++ ranger/uninstall.sh
@@ -1,3 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 unlink ~/.config/ranger/rc.conf
+unlinl ~/.config/ranger/rifle.conf
