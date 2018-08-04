35a13ad9ed42c11573e653059a8ec74853ecfab6
Nelson Estevão <nelsonmestevao@gmail.com>
2018-08-05 00:57:52 +0100

    Remove xmonad

--- xmonad/uninstall.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-echo "Installing XMonad..."
-sudo apt-get remove xmonad libghc-xmonad-dev libghc-xmonad-contrib-dev xmobar xcompmgr nitrogen stalonetray moreutils synapse ssh-askpass-gnome suckless-tools
-
-unlink  ~/.xmonad/xmonad.hs
