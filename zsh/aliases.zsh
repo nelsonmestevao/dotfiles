287dea9d5aaa3074e378c5230fbb28d61df028c9
Nelson Estevão <nelsonmestevao@gmail.com>
2018-08-17 15:19:38 +0100

    [zsh]: :partly_sunny: Add weather alias

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -5,23 +5,27 @@
 alias dot="cd ~/.dotfiles && nvim ."
 alias spl="cd ~/.spells && nvim ."
 
-alias v="nvim"
-alias vim="nvim"
-
 alias c="cd ~/Code && ls -l"
 alias s="cd ~/Snippets && ls -l"
 
-alias r="ranger ."
+alias v="nvim"
+alias vim="nvim"
 
-alias cv="cd ~/Code/curriculum-vitae/ && ls -l"
-alias ocv="cd ~/Code/online-curriculum/ && ls -l"
+alias r="ranger ."
+alias :q="exit"
+
+alias joão="date -d '+2 hour' '+%T' && weather Tampere Finland 0"
+alias carolina="date -d '+1 hour' '+%T' && weather Brussels Belgium 0"
 
 alias backup="git add --all && git commit -am ':wrench: [WIP] Done for today, cya tomorrow [ci skip] :wave:' && git push"
 alias bsc=backup
 
-alias um='cd ~/Code/uminho/'
-alias li1='cd ~/Code/uminho/1st/1/Laboratórios\ de\ Informática\ I/Project'
-alias li2='cd ~/Code/uminho/1st/2/Laboratórios\ de\ Informática\ II/Project'
+alias cv="cd ~/Code/curriculum-vitae/ && ls -l"
+alias ocv="cd ~/Code/online-curriculum/ && ls -l"
 
-alias svnl='svn log | less'
-alias svnd='svn diff'
+alias um="cd ~/Code/uminho/"
+alias li1="cd ~/Code/uminho/1st/1/Laboratórios\ de\ Informática\ I/Project"
+alias li2="cd ~/Code/uminho/1st/2/Laboratórios\ de\ Informática\ II/Project"
+
+alias svnl="svn log | less"
+alias svnd="svn diff"
