7a8feafc9c02412368502f7fb422df924d69925a
Nelson Estevão <nelsonmestevao@gmail.com>
2018-08-17 16:54:40 +0100

    [zsh]: Add mkcd function

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -4,6 +4,11 @@ function gi() {
   curl -L -s https://www.gitignore.io/api/$@ ;
 }
 
+function mkcd() {
+  mkdir -p $@ ;
+  cd $@ ;
+}
+
 function weather() {
   curl 'wttr.in/~'${1:-Braga}'+'$2'?'$3 ;
 }
