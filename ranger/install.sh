52fead1ebe9861ef69f915e05ac5a44d9225a73e
Nelson Estevão <nelsonmestevao@gmail.com>
2018-08-18 15:59:30 +0100

    Update ranger

--- ranger/install.sh
+++ ranger/install.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env sh
 
-git clone git@github.com:alexanderjeurissen/ranger_devicons.git
-
 ln -sfT ~/.dotfiles/ranger/rc.conf ~/.config/ranger/rc.conf
 ln -sfT ~/.dotfiles/ranger/rifle.conf ~/.config/ranger/rifle.conf
 
-rm -rf ranger_devicons
