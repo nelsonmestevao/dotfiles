10f1b0355ff50b5a0e349df540aa444927bf745c
Nelson Estevão <nelsonmestevao@gmail.com>
2018-09-14 02:23:17 +0100

    Update i3 configuration

--- i3/install.sh
+++ i3/install.sh
@@ -1,11 +1,3 @@
 #!/usr/bin/env sh
 
-git clone --recursive https://github.com/jaagr/polybar make-polybar
-mkdir polybar/build
-cd polybar/build
-cmake ..
-sudo make install
-cd ..
-rm -rf make-polybar
-
 ln -sfT ~/.dotfiles/i3/config ~/.i3/config
