74d941c46c4c1b40cec864a22657c0c07cdd9337
Nelson Estevão <nelsonmestevao@gmail.com>
2018-09-15 15:52:18 +0100

    [zsh]: Improve aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -2,11 +2,11 @@
 
 #set -o vi
 
-alias dot="cd ~/.dotfiles && nvim ."
-alias spl="cd ~/.spells && nvim ."
+alias dot="cd $HOME/.dotfiles && nvim ."
+alias spl="cd $HOME~/.spells && nvim ."
 
-alias c="cd ~/Code && ls -l"
-alias s="cd ~/Snippets && ls -l"
+alias c="cd $HOME~/Code && ls -l"
+alias s="cd $HOME~/Snippets && ls -l"
 
 alias v="nvim"
 alias vim="nvim"
@@ -19,12 +19,12 @@ alias carolina="date -d '+1 hour' '+%T' && weather Brussels Belgium 0"
 
 alias bsc=backup
 
-alias cv="cd ~/Code/curriculum-vitae/ && ls -l"
-alias ocv="cd ~/Code/online-curriculum/ && ls -l"
+alias cv="cd $HOME/Code/curriculum-vitae/ && ls -l"
+alias ocv="cd $HOME/Code/online-curriculum/ && ls -l"
 
-alias um="cd ~/Code/uminho/"
-alias li1="cd ~/Code/uminho/1st/1/Laboratórios\ de\ Informática\ I/Project"
-alias li2="cd ~/Code/uminho/1st/2/Laboratórios\ de\ Informática\ II/Project"
+alias um="cd $HOME/Code/uminho/"
+alias li1="cd $HOME/Code/uminho/1st/1/Laboratórios\ de\ Informática\ I/Project"
+alias li2="cd $HOME/Code/uminho/1st/2/Laboratórios\ de\ Informática\ II/Project"
 
 alias svnl="svn log | less"
 alias svnd="svn diff"
