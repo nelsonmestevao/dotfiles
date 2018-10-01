3e93d801ade91a7e18e69fde3d3d17152464c0f4
Nelson Estevão <nelsonmestevao@gmail.com>
2018-10-02 00:52:33 +0100

    [zsh]: Add new aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,23 +1,10 @@
 # ALIASES
 
-#set -o vi
-
 alias dot="cd $HOME/.dotfiles && nvim ."
 alias spl="cd $HOME/.spells && nvim ."
 
 alias c="cd $HOME/Code && ls -l"
 alias s="cd $HOME/Snippets && ls -l"
-
-alias v="nvim"
-alias vim="nvim"
-
-alias r="ranger ."
-alias :q="exit"
-
-alias joão="date -d '+2 hour' '+%T' && weather Tampere Finland 0"
-alias carolina="date -d '+1 hour' '+%T' && weather Brussels Belgium 0"
-
-alias bsc=backup
 
 alias cv="cd $HOME/Code/curriculum-vitae/ && ls -l"
 alias ocv="cd $HOME/Code/online-curriculum/ && ls -l"
@@ -26,8 +13,20 @@ alias um="cd $HOME/Code/uminho/"
 alias li1="cd $HOME/Code/uminho/1st/1/Laboratórios\ de\ Informática\ I/Project"
 alias li2="cd $HOME/Code/uminho/1st/2/Laboratórios\ de\ Informática\ II/Project"
 
-alias svnl="svn log | less"
-alias svnd="svn diff"
+alias v="nvim"
+alias vim="nvim"
+
+alias r="ranger"
+alias p="sudo pacman"
+alias y="yaourt"
+alias ka="killall"
+alias :q="exit"
+
+alias bsc=backup
+
+alias joão="date -d '+2 hour' '+%T' && weather Tampere Finland 0"
+alias carolina="date -d '+1 hour' '+%T' && weather Brussels Belgium 0"
 
 alias miei="slack-term --config $HOME/.slack-miei"
 alias cesium="xrandr --output HDMI1 --auto --above eDP1"
+
