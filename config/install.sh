2a5e5fc9209f607f90809f948f0d29ab23e6e166
Nelson Estevão <nelsonmestevao@gmail.com>
2018-10-11 17:59:10 +0100

    [Okular]: Add okular config for annotation

--- config/install.sh
+++ config/install.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
 
 ln -sfT ~/.dotfiles/config/profile ~/.profile
+ln -sfT ~/.dotfiles/config/okularpartrc ~/.config/okularpartrc
 
 sudo cp ~/.dotfiles/config/50-noto-color-emoji.conf /etc/fonts/conf.d/
 
