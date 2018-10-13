67ed314696dcc6e39499872815e4083df0fc0f11
Marco Gonçalves <marcomatiaspg@gmail.com>
2018-10-13 20:11:26 +0100

    :wrench: [WIP] Done for today, cya tomorrow [ci skip] :wave:

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -3,7 +3,6 @@
 alias dot="cd $HOME/.dotfiles && nvim ."
 alias spl="cd $HOME/.spells && nvim ."
 
-alias c="cd $HOME/Code && ls -l"
 alias s="cd $HOME/Snippets && ls -l"
 
 alias cv="cd $HOME/Code/curriculum-vitae/ && ls -l"
@@ -14,11 +13,8 @@ alias li1="cd $HOME/Code/uminho/1st/1/Laboratórios\ de\ Informática\ I/Project
 alias li2="cd $HOME/Code/uminho/1st/2/Laboratórios\ de\ Informática\ II/Project"
 
 alias v="nvim"
-alias vim="nvim"
 
 alias r="ranger"
-alias p="sudo pacman"
-alias y="yaourt"
 alias ka="killall"
 alias :q="exit"
 
@@ -29,4 +25,14 @@ alias carolina="date -d '+1 hour' '+%T' && weather Brussels Belgium 0"
 
 alias miei="slack-term --config $HOME/.slack-miei"
 alias cesium="xrandr --output HDMI1 --auto --above eDP1"
+#bota Remedio
 
+alias um="cd ~/Universidade/"
+alias bsc="git add --all && git commit -m ':wave: :speedboat: :wave:' && git push"
+alias c="clear"
+alias p="cd ~/Projects/"
+alias sd="cd ~/Universidade/SD/"
+#PDF 
+alias open="okular"
+alias atomic="cd ~/Projects/atomic/"
+alias sensors="watch -n 1 -d sensors"
