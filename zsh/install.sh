18e41ff0031e26787218c6555a7759d112c5de24
Nelson Estevão <nelsonmestevao@gmail.com>
2018-11-16 12:10:17 +0000

    [zsh]: Add zplug

--- zsh/install.sh
+++ zsh/install.sh
@@ -3,10 +3,9 @@
 echo "Installing ZSH with OH-MY-ZSH..."
 sudo pacman -Syu zsh --needed --noconfirm
 
+# oh-my-zsh installation
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-git clone https://github.com/bhilburn/powerlevel9k.git             "$HOME/.oh-my-zsh/custom/themes/powerlevel9k"
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting"
 git clone https://github.com/zsh-users/zsh-autosuggestions         "$HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions"
 
 ln -sfT "$HOME/.dotfiles/zsh/zshrc"   "$HOME/.zshrc"
---
+++ zsh/plugins/syntax-highlighting.zsh
@@ -0,0 +1,19 @@
+# zsh-syntax-highlighting
+ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets pattern)
+ZSH_HIGHLIGHT_PATTERNS+=("rm -rf *" "fg=white,bold,bg=red")
+typeset -A ZSH_HIGHLIGHT_STYLES
+ZSH_HIGHLIGHT_STYLES[path]="fg=white"
+ZSH_HIGHLIGHT_STYLES[path_pathseparator]="fg=grey"
+ZSH_HIGHLIGHT_STYLES[alias]="fg=cyan"
+ZSH_HIGHLIGHT_STYLES[builtin]="fg=cyan"
+ZSH_HIGHLIGHT_STYLES[function]="fg=cyan"
+ZSH_HIGHLIGHT_STYLES[command]='fg=green'
+ZSH_HIGHLIGHT_STYLES[precommand]='fg=green'
+ZSH_HIGHLIGHT_STYLES[hashed-command]='fg=green'
+ZSH_HIGHLIGHT_STYLES[commandseparator]="fg=yellow"
+ZSH_HIGHLIGHT_STYLES[redirection]="fg=magenta"
+ZSH_HIGHLIGHT_STYLES[bracket-level-1]="fg=cyan,bold"
+ZSH_HIGHLIGHT_STYLES[bracket-level-2]="fg=green,bold"
+ZSH_HIGHLIGHT_STYLES[bracket-level-3]="fg=magenta,bold"
+ZSH_HIGHLIGHT_STYLES[bracket-level-4]="fg=yellow,bold"
+
