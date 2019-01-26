f8f725df9640061928e7e123ee5d2aab2ec65052
Nelson Estevão <nelsonmestevao@gmail.com>
2019-01-26 13:50:53 +0000

    [asdf]: Update installation script
    This commit adds the plugins, versions for the necessary languages that
    I have to use.

--- asdf/install.sh
+++ asdf/install.sh
@@ -1,7 +1,24 @@
 #!/usr/bin/env sh
 
-git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.5.1
+git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.6.3
 
+echo "Symling .asdfrc..."
 ln -sfT ~/.dotfiles/asdf/asdfrc        ~/.asdfrc
+
+echo "Symling .tool-versions..."
 ln -sfT ~/.dotfiles/asdf/tool-versions ~/.tool-versions
+
+echo "Update asdf itself..."
+asdf update
+
+echo "Add asdf plugins..."
+asdf plugin-add R
+asdf plugin-add golang
+asdf plugin-add haskell
+asdf plugin-add julia
+asdf plugin-add nodejs
+asdf plugin-add ruby
+
+echo "Update all plugins..."
+asdf plugin-update --all
 
