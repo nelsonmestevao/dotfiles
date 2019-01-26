5c3cecf7a6027f8fa77116c828d978f4d501062e
Nelson Estevão <nelsonmestevao@gmail.com>
2019-01-26 16:52:08 +0000

    [julia]: Add improve startup.jl

--- julia/startup.jl
+++ julia/startup.jl
@@ -1,2 +1,18 @@
+using Pkg
+
+PKGS = ["OhMyREPL"
+        , "CSV"
+        , "DataFrames"
+        , "Missings"
+        , "Parsers"
+        , "RData"
+     ]
+
+for pkg in PKGS
+  if pkg ∉ keys(Pkg.installed())
+    Pkg.add(pkg)
+  end
+end
+
 using OhMyREPL
 
