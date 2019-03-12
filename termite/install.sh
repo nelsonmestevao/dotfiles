abfffe42c20e87736982a3e9901f81caa488bf7c
Nelson Estevão <nelsonmestevao@gmail.com>
2019-03-12 15:37:47 +0000

    [termite]: Update themes and structure

--- termite/install.sh
+++ termite/install.sh
@@ -10,6 +10,8 @@ _install termite
 
 echo_info "Symlink termite config"
 mkdir -p ~/.config/termite
-ln -sfT ~/.dotfiles/termite/config ~/.config/termite/config
+ln -sfT ~/.dotfiles/termite/option ~/.config/termite/option
+ln -sfT ~/.dotfiles/termite/theme ~/.config/termite/theme
+ln -sfT ~/.dotfiles/termite/color/ ~/.config/termite/color
 
 echo_done "Termite configuration!"
