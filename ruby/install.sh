eb76d7b86c554c9d3c6b219601105a477748c3de
Nelson Estevão <nelsonmestevao@gmail.com>
2019-05-09 12:10:17 +0100

    [ruby]: Add configuration file

--- ruby/install.sh
+++ ruby/install.sh
@@ -8,4 +8,7 @@
 echo_info "Symling .default-gems..."
 ln -sfT ~/.dotfiles/ruby/default-gems ~/.default-gems
 
+echo_info "Symling .irbrc..."
+ln -sfT ~/.dotfiles/ruby/irbrc ~/.irbrc
+
 echo_done "Ruby configuration!"
