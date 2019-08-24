f95b9a4fb0a8b7973d686328044b8eb9e201b867
Nelson Estevão <nelsonmestevao@gmail.com>
2019-08-24 16:15:44 +0100

    Update zsh organization and bug fix on nodejs

--- nodejs/install.sh
+++ nodejs/install.sh
@@ -9,6 +9,7 @@ echo "Add NodeJS keys..."
 bash ~/.asdf/plugins/nodejs/bin/import-release-team-keyring
 
 echo "Symling .default-npm-packages..."
-ln -sfT ~/.dotfiles/ruby/default-npm-packages ~/.default-npm-packages
+ln -sfT ~/.dotfiles/nodejs/default-npm-packages ~/.default-npm-packages
 
 echo_done "NodeJS configuration!"
+
