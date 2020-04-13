a3efc33d10628af5679b13aef3f698fbffa9aea7
Nelson Estevão <nelsonmestevao@gmail.com>
2020-04-13 13:34:35 +0100

    [zsh]: Migrate to p10k

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -6,7 +6,7 @@ plugins=(
   extract
   git
   # golang #
-  mercurial
+  # mercurial #
   # npm #
   tmux
   tmuxinator
@@ -17,39 +17,39 @@ plugins=(
   zsh-syntax-highlighting
 )
 
-source $DOTFILES/zsh/plugins/syntax-highlighting.zsh
+# source $DOTFILES/zsh/plugins/syntax-highlighting.zsh
 
 ### zplug #####################################################################
 
 # Check if zplug is installed
-if [[ ! -d ~/.zplug ]]; then
-  curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
-  source ~/.zplug/init.zsh && zplug update --self
-fi
+# if [[ ! -d ~/.zplug ]]; then
+#   curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
+#   source ~/.zplug/init.zsh && zplug update --self
+# fi
 
-source ~/.zplug/init.zsh
+# source ~/.zplug/init.zsh
 
 # Plugins List
 
-zplug "zsh-users/zsh-syntax-highlighting"
+# zplug "zsh-users/zsh-syntax-highlighting"
 
 # -----------------------------------------------------------------------------
 
 # Themes List
 
-zplug "bhilburn/powerlevel9k", use:powerlevel9k.zsh-theme
+# zplug "bhilburn/powerlevel9k", use:powerlevel9k.zsh-theme
 
 # -----------------------------------------------------------------------------
 
 # Install packages that have not been installed yet
-if ! zplug check --verbose; then
-  printf "Install? [y/N]: "
-  if read -q; then
-    echo
-    zplug install
-  else
-    echo
-  fi
-fi
+# if ! zplug check --verbose; then
+#   printf "Install? [y/N]: "
+#   if read -q; then
+#     echo
+#     zplug install
+#   else
+#     echo
+#   fi
+# fi
 
-zplug load
+# zplug load
