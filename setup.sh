299abb9ed22249093988f7792ded1cb5a7d7b37e
Nelson Estevão <nelsonmestevao@gmail.com>
2020-06-24 14:35:25 +0100

    Add setup script

---
+++ setup.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+git clone git@github.com/nelsonmestevao/dotfiles.git ~/.dotfiles
+git clone git@github.com/nelsonmestevao/spells.git   ~/.spells
+
+cd ~/.dotfiles
+make install
+
