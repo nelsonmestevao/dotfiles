7492e11926fb77f5a187954e95234f4495218e49
Nelson Estevão <nelsonmestevao@gmail.com>
2020-12-28 11:35:10 +0000

    [zsh]: Clean up zsh folder
    See https://blog.jonlu.ca/posts/speeding-up-zsh for zprof.
    
    References
    ==========
    
    - https://blog.mattclemente.com/2020/06/26/oh-my-zsh-slow-to-load.html

--- zsh/theme.zsh
+++ zsh/theme.zsh
@@ -2,13 +2,9 @@
 export ZSH=~/.oh-my-zsh
 export TERM="xterm-256color"
 
-# POWERLEVEL9K_MODE='awesome-fontconfig'
-# ZSH_THEME="powerlevel9k/powerlevel9k"
 ZSH_THEME="powerlevel10k/powerlevel10k"
 
 source $ZSH/oh-my-zsh.sh
-
-# source $DOTFILES/zsh/themes/powerlevel9k.zsh
 
 # To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
 source $DOTFILES/zsh/themes/p10k.zsh
