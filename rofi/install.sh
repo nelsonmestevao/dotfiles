3d56af7683aa9cb90a5c60970e6a44d8d2472452
Nelson Estevão <nelsonmestevao@gmail.com>
2021-04-04 19:32:59 +0100

    [rofi]: Upgrade configuration

--- rofi/install.sh
+++ rofi/install.sh
@@ -3,10 +3,13 @@
 BASE_DIR=$(dirname "${BASH_SOURCE[0]:-$0}")
 cd "${BASE_DIR}/.." || exit 127
 
+# shellcheck source=../scripts/utils.sh
+. scripts/utils.sh
 # shellcheck source=../scripts/extras.sh
 . scripts/extras.sh
 
-install_package rofi
+ask_for_sudo
+#install_package rofi
 
 mkdir -p ~/.config/rofi
-symlink ~/.dotfiles/rofi/config ~/.config/rofi/config
+symlink ~/.dotfiles/rofi/config.rasi ~/.config/rofi/config.rasi
