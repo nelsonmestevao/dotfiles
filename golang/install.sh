31f631f78ef7173a8a1e2638ab7636cdff0c9a62
Nelson Estevão <nelsonmestevao@gmail.com>
2021-04-05 16:00:55 +0100

    [golang]: Add go default pkgs

---
+++ golang/install.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+BASE_DIR=$(dirname "${BASH_SOURCE[0]:-$0}")
+cd "${BASE_DIR}/.." || exit 127
+
+# shellcheck source=../scripts/extras.sh
+. scripts/extras.sh
+
+symlink ~/.dotfiles/golang/default-golang-pkgs ~/.default-golang-pkgs
+
