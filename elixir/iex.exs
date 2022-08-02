7825371c7d1b9f81fcd47a276044dfcca26b6309
Nelson Estevão <nelsonmestevao@proton.me>
2022-08-02 13:38:06 +0100

    [elixir]: Make logging sane again
    Tip from
    https://elixirstream.dev/tips/8b6bb75f-239f-4c37-87d8-e9be7652954b.

--- elixir/iex.exs
+++ elixir/iex.exs
@@ -42,3 +42,8 @@ IEx.configure(
   ],
   width: 80
   )
+
+if function_exported?(Mix, :__info__, 1) and Mix.env() == :dev do
+  # if statement guards you from running it in prod, which could result in loss of logs.
+  Logger.configure_backend(:console, device: Process.group_leader())
+end
