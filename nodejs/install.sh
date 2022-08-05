fad639affe8479608cec456d07c4b584297dda70
Nelson Estevão <nelsonmestevao@proton.me>
2022-08-05 23:16:25 +0100

    [nodejs]: Clean up nodejs keys

--- nodejs/install.sh
+++ nodejs/install.sh
@@ -8,6 +8,4 @@ cd "${BASE_DIR}/.." || exit 127
 # shellcheck source=../scripts/extras.sh
 . scripts/extras.sh
 
-execute "bash $HOME/.asdf/plugins/nodejs/bin/import-release-team-keyring" "Adding nodejs gpg keys..."
-
 symlink "$HOME/.dotfiles/nodejs/default-npm-packages" "$HOME/.default-npm-packages"
