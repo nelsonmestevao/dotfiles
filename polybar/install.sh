fa5cc7028036cee0c6ecaaeb0a9f5ecaf40a86ec
Nelson Estevão <nelsonmestevao@proton.me>
2022-08-06 01:49:49 +0100

    [i3wm]: Refactor and clean up

---
+++ polybar/install.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+BASE_DIR=$(dirname "${BASH_SOURCE[0]:-$0}")
+cd "${BASE_DIR}/.." || exit 127
+
+# shellcheck source=../scripts/extras.sh
+. scripts/extras.sh
+# shellcheck source=../scripts/utils.sh
+. scripts/utils.sh
+
+ask_for_sudo
+
+install_package polybar
+
