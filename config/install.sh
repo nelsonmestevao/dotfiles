d61921b0660332e54ea56597c424d4e060af10e0
Nelson Estevão <nelsonmestevao@proton.me>
2022-08-11 15:48:37 +0100

    [nitrogen]: Add nitrogen configuration setup

--- config/install.sh
+++ config/install.sh
@@ -30,17 +30,6 @@ download_fonts() {
   sudo cp ~/.dotfiles/config/50-noto-color-emoji.conf /etc/fonts/conf.d/
 }
 
-download_wallpapers() {
-  mkdir -p ~/Pictures
-
-  curl -o ~/Pictures/desktop.jpg "https://images.unsplash.com/photo-1533759233673-2d30c9bc1e93"
-
-  curl -o ~/Pictures/login.jpg "https://images.unsplash.com/photo-1548354911-457c10eaf236"
-
-  sudo mv ~/Pictures/desktop.jpg /usr/share/backgrounds/i3_default_background.jpg
-
-}
-
 symlink ~/.dotfiles/config/profile ~/.profile
 
 symlink ~/.dotfiles/config/user-dirs.dirs ~/.config/user-dirs.dirs
@@ -51,5 +40,3 @@ symlink ~/.dotfiles/config/shutdown.desktop ~/.local/share/applications/shutdown
 symlink ~/.dotfiles/config/logout.desktop ~/.local/share/applications/logout.desktop
 
 execute download_fonts "Downloading fonts..."
-
-execute download_wallpapers "Downloading Wallpapers..."
---
+++ nitrogen/install.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+#set -Eeuo pipefail
+
+BASE_DIR=$(dirname "${BASH_SOURCE[0]:-$0}")
+cd "${BASE_DIR}/.." || exit 127
+
+# shellcheck source=../scripts/extras.sh
+. scripts/extras.sh
+# shellcheck source=../scripts/utils.sh
+. scripts/utils.sh
+
+download_wallpapers() {
+  mkdir -p ~/Pictures
+
+  curl -o ~/Pictures/desktop.jpg "https://images.unsplash.com/photo-1533759233673-2d30c9bc1e93"
+
+  curl -o ~/Pictures/login.jpg "https://images.unsplash.com/photo-1548354911-457c10eaf236"
+
+  sudo cp ~/Pictures/desktop.jpg /usr/share/backgrounds/i3_default_background.jpg
+}
+
+ask_for_sudo
+
+install_package nitrogen
+
+mkdir -p "$HOME/.config/nitrogen"
+
+symlink "$HOME/.dotfiles/nitrogen/nitrogen.cfg" "$HOME/.config/nitrogen/nitrogen.cfg"
+symlink "$HOME/.dotfiles/nitrogen/bg-saved.cfg" "$HOME/.config/nitrogen/bg-saved.cfg"
+
+execute download_wallpapers "Downloading Wallpapers..."
