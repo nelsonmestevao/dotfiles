3a0e1e32dccab54d0102fc7f83995ee8f80f9b28
Nelson Estevão <nelsonmestevao@proton.me>
2022-09-19 18:24:29 +0100

    [haskell]: Upgrade haskell setup with extensions and tools

--- haskell/install.sh
+++ haskell/install.sh
@@ -6,10 +6,37 @@ cd "${BASE_DIR}/.." || exit 127
 # shellcheck source=../scripts/extras.sh
 . scripts/extras.sh
 
-function download_ghcup() {
-  curl --proto '=https' --tlsv1.2 -sSf https://get-ghcup.haskell.org | sh
+function install_ghcup() {
+  curl --proto '=https' --tlsv1.2 -sSf https://get-ghcup.haskell.org | BOOTSTRAP_HASKELL_NONINTERACTIVE=1 sh
+}
+
+function install_libraries() {
+  declare -a libs=(HUnit gloss)
+
+  for lib in "${libs[@]}"; do
+    cabal install --lib $lib
+  done
+}
+
+function install_tools() {
+  declare -a tools=(hlint hindent)
+
+  for tool in "${tools[@]}"; do
+    cabal install --lib $tool
+  done
+}
+
+function install_extensions() {
+  declare -a extensions=("haskell.haskell")
+
+  for extension in "${extensions[@]}"; do
+    code --install-extension $extension
+  done
 }
 
 symlink "$HOME/.dotfiles/haskell/ghci" "$HOME/.ghci"
 
-download_ghcup
+execute install_ghcup "Installing GHCup..."
+execute install_libraries "Installing Haskell libraries..."
+execute install_tools "Installing Haskell tools..."
+execute install_extensions "Install VS Code Haskell extensions..."
