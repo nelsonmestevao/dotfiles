7a1c76343002d0ccf3e9b832e4eeb560cff60878
Nelson Estevão <nelsonmestevao@proton.me>
2023-07-20 10:26:49 +0100

    [i3wm]: add i3ipc package to dependencies

--- i3wm/install.sh
+++ i3wm/install.sh
@@ -5,12 +5,16 @@
 BASE_DIR=$(dirname "${BASH_SOURCE[0]:-$0}")
 cd "${BASE_DIR}/.." || exit 127
 
+# shellcheck source=../scripts/distro.sh
+. scripts/distro.sh
 # shellcheck source=../scripts/execs.sh
 . scripts/execs.sh
 # shellcheck source=../scripts/utils.sh
 . scripts/utils.sh
 
 ask_for_sudo
+
+install_package python-i3ipc
 
 mkdir -p "$HOME/.config/i3"
 symlink "$HOME/.dotfiles/i3wm/config" "$HOME/.config/i3/config"
