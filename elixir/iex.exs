000e00c0a61a5a17ae484b9d4a081e36c6fe67d4
Nelson Estevão <nelsonmestevao@proton.me>
2024-07-14 18:07:37 +0100

    [elixir]: add application and env to prompt

--- elixir/iex.exs
+++ elixir/iex.exs
@@ -1,13 +1,27 @@
 timestamp = fn ->
-  {_date, {hour, minute, _second}} = :calendar.local_time
+  {_date, {hour, minute, _second}} = :calendar.local_time()
+
   [hour, minute]
-  |> Enum.map(&(String.pad_leading(Integer.to_string(&1), 2, "0")))
+  |> Enum.map(&String.pad_leading(Integer.to_string(&1), 2, "0"))
   |> Enum.join(":")
 end
 
 elixir_icon = fn ->
-  "#{IO.ANSI.magenta}#{IO.ANSI.reset}"
+  "#{IO.ANSI.magenta()}#{IO.ANSI.reset()}"
 end
+
+format_env = fn
+  :dev -> "#{IO.ANSI.green()}dev#{IO.ANSI.reset()}"
+  :prod -> "#{IO.ANSI.red()}prod#{IO.ANSI.reset()}"
+  :test -> "#{IO.ANSI.cyan()}test#{IO.ANSI.reset()}"
+end
+
+prefix =
+  if function_exported?(Mix, :__info__, 1) and Mix.State in :ets.all() do
+    "#{IO.ANSI.light_blue()}#{Mix.Project.config()[:app]}#{IO.ANSI.reset()} (#{format_env.(Mix.env())}) "
+  else
+    "#{IO.ANSI.green()}%prefix#{IO.ANSI.reset()} "
+  end
 
 IEx.configure(
   colors: [
@@ -16,24 +30,24 @@ IEx.configure(
       atom: :light_cyan,
       string: :light_green,
       boolean: :red,
-      nil: [:magenta, :bright],
+      nil: [:magenta, :bright]
     ],
     ls_directory: :cyan,
     ls_device: :yellow,
     doc_code: :green,
     doc_inline_code: :magenta,
     doc_headings: [:cyan, :underline],
-    doc_title: [:cyan, :bright, :underline],
+    doc_title: [:cyan, :bright, :underline]
   ],
   default_prompt:
-  "#{IO.ANSI.green}%prefix#{IO.ANSI.reset} " <>
-    "[#{IO.ANSI.magenta}#{timestamp.()}#{IO.ANSI.reset} " <>
-      "#{IO.ANSI.cyan}%counter#{IO.ANSI.reset}] #{elixir_icon.()}",
+    "#{prefix}" <>
+      "[#{IO.ANSI.magenta()}#{timestamp.()}#{IO.ANSI.reset()} " <>
+      "#{IO.ANSI.cyan()}%counter#{IO.ANSI.reset()}] #{elixir_icon.()}",
   alive_prompt:
-  "#{IO.ANSI.green}%prefix#{IO.ANSI.reset} " <>
-    "(#{IO.ANSI.yellow}%node#{IO.ANSI.reset}) " <>
-      "[#{IO.ANSI.magenta}#{timestamp.()}#{IO.ANSI.reset} " <>
-        "#{IO.ANSI.cyan}%counter#{IO.ANSI.reset}] #{elixir_icon.()}",
+    "#{prefix}" <>
+      "(#{IO.ANSI.yellow()}%node#{IO.ANSI.reset()}) " <>
+      "[#{IO.ANSI.magenta()}#{timestamp.()}#{IO.ANSI.reset()} " <>
+      "#{IO.ANSI.cyan()}%counter#{IO.ANSI.reset()}] #{elixir_icon.()}",
   history_size: 50,
   inspect: [
     pretty: true,
@@ -42,7 +56,7 @@ IEx.configure(
     custom_options: [sort_maps: true]
   ],
   width: 80
-  )
+)
 
 defmodule :_utils do
   defdelegate bye(), to: System, as: :halt
@@ -55,4 +69,8 @@ import :_utils
 if function_exported?(Mix, :__info__, 1) and Mix.State in :ets.all() and Mix.env() == :dev do
   # if statement guards you from running it in prod, which could result in loss of logs.
   Logger.configure_backend(:console, device: Process.group_leader())
+end
+
+if function_exported?(Phoenix.Endpoint, :host, 0) do
+  IO.puts("Hello, Man!")
 end
