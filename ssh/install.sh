23536d787d2b3f431fda2082a45c57fd705beb08
Nelson Estevão <nelsonmestevao@proton.me>
2024-07-23 21:42:44 +0100

    [ssh]: fix symlinking

--- ssh/install.sh
+++ ssh/install.sh
@@ -3,6 +3,8 @@
 BASE_DIR=$(dirname "${BASH_SOURCE[0]:-$0}")
 cd "${BASE_DIR}/.." || exit 127
 
+# shellcheck source=../scripts/distro.sh
+. scripts/distro.sh
 # shellcheck source=../scripts/execs.sh
 . scripts/execs.sh
 
