0494ab6b0cf1277028a9418d5548b75b40144e1d
Nelson Estevão <nelson.estevao@remote.com>
2024-07-23 16:54:01 +0100

    [wezterm]: add basic shortcut configuration

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,9 +1,17 @@
+--
+-- ██╗    ██╗███████╗███████╗████████╗███████╗██████╗ ███╗   ███╗
+-- ██║    ██║██╔════╝╚══███╔╝╚══██╔══╝██╔════╝██╔══██╗████╗ ████║
+-- ██║ █╗ ██║█████╗    ███╔╝    ██║   █████╗  ██████╔╝██╔████╔██║
+-- ██║███╗██║██╔══╝   ███╔╝     ██║   ██╔══╝  ██╔══██╗██║╚██╔╝██║
+-- ╚███╔███╔╝███████╗███████╗   ██║   ███████╗██║  ██║██║ ╚═╝ ██║
+--  ╚══╝╚══╝ ╚══════╝╚══════╝   ╚═╝   ╚══════╝╚═╝  ╚═╝╚═╝     ╚═╝
+-- A GPU-accelerated cross-platform terminal emulator
 local wezterm = require('wezterm')
 
-local config = wezterm.config_builder()
+local config = {}
 
 config.window_decorations = "RESIZE"
-config.window_background_opacity = 0.9
+config.window_background_opacity = 0.85
 config.macos_window_background_blur = 50
 
 config.font = wezterm.font('JetBrainsMono NF')
@@ -11,5 +19,31 @@ config.font_size = 18
 
 config.enable_scroll_bar = false
 config.enable_tab_bar = false
+
+config.leader = {
+  key = 'a',
+  mods = 'CTRL',
+  timeout_milliseconds = 1000
+}
+
+config.keys = {
+  {
+    key = 's',
+    mods = 'LEADER',
+    action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' },
+  },
+  {
+    key = 'v',
+    mods = 'LEADER',
+    action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' },
+  },
+  {
+    key = 'p',
+    mods = 'LEADER',
+    action = wezterm.action.PaneSelect {
+      mode = 'Activate',
+    },
+  },
+}
 
 return config
