0e456c9c76db9c907b38f83fbe244b4145dcb677
Nelson Estevão <nelson.estevao@remote.com>
2024-07-24 20:05:29 +0100

    [zsh]: start using yazi as terminal file manager

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -21,6 +21,15 @@ alias ls="eza --icons"
 alias ka="killall"
 alias :q="exit"
 
+function yy() {
+	local tmp="$(mktemp -t "yazi-cwd.XXXXXX")"
+	yazi "$@" --cwd-file="$tmp"
+	if cwd="$(cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
+		cd -- "$cwd"
+	fi
+	rm -f -- "$tmp"
+}
+
 alias o=open
 alias f=find-file
 
