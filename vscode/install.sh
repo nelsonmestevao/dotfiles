3a85709e898946c56def65151b763e0aa3ea216e
Nelson Estevão <nelsonmestevao@proton.me>
2024-07-25 21:38:46 +0100

    [vscode]: upgrade VS Code extensions

--- vscode/install.sh
+++ vscode/install.sh
@@ -3,28 +3,13 @@
 BASE_DIR=$(dirname "${BASH_SOURCE[0]:-$0}")
 cd "${BASE_DIR}/.." || exit 127
 
+# shellcheck source=../scripts/distro.sh
+. scripts/distro.sh
 # shellcheck source=../scripts/execs.sh
 . scripts/execs.sh
 
 function install_extensions() {
-  declare -a extensions=(
-    GitHub.github-vscode-theme
-    # JakeBecker.elixir-ls
-    lexical-lsp.lexical
-    MS-vsliveshare.vsliveshare
-    ms-vscode.makefile-tools
-    PKief.material-icon-theme
-    SimonSiefke.svg-preview
-    bradlc.vscode-tailwindcss
-    eamodio.gitlens
-    streetsidesoftware.code-spell-checker
-    haskell.haskell
-    justusadam.language-haskell
-    miguelsolorio.fluent-icons
-    # pantajoe.vscode-elixir-credo
-    victorbjorklund.phoenix
-    vscodevim.vim
-  )
+  declare -a extensions=($(cat ./vscode/extensions.txt | tr "\n" " "))
 
   for extension in "${extensions[@]}"; do
     code --install-extension $extension
