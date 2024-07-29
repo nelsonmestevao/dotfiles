ecd16307c02fcb29e5702993d12688f03c606205
Nelson Estevão <nelson.estevao@remote.com>
2024-07-29 15:07:18 +0100

    [wezterm]: use Google theme

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -14,6 +14,16 @@ config.window_decorations = "RESIZE"
 config.window_background_opacity = 0.85
 config.macos_window_background_blur = 50
 
+local function get_color_scheme_by(appearance)
+  if appearance:find "Dark" then
+    return "Google Dark (base16)"
+  else
+    return "Google Light (base16)"
+  end
+end
+
+config.color_scheme = get_color_scheme_by(wezterm.gui.get_appearance())
+
 config.font = wezterm.font('JetBrainsMono NF')
 config.font_size = 18
 
