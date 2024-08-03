ec0d645eafc2e821ab88d58d0a9f1822b52fac16
Nelson Estevão <nelsonmestevao@proton.me>
2024-08-03 01:50:26 +0100

    [gnome]: save gnome shortcuts configuration

--- gnome/save.sh
+++ gnome/save.sh
@@ -1,1 +1,2 @@
 dconf dump / | sed -n '/\[org.gnome.settings-daemon.plugins.media-keys/,/^$/p' > ~/.dotfiles/gnome/user.conf
+dconf dump / | sed -n '/\[org.gnome.desktop.wm.keybindings/,/^$/p' >> ~/.dotfiles/gnome/user.conf
