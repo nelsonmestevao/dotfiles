5c4f515006879fd403b3681183dc4ae8a5893454
Nelson Estevão <nelsonmestevao@proton.me>
2024-08-03 21:26:25 +0100

    [wezterm]: increase background opacity in linux since cannot blur

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -11,7 +11,7 @@ local wezterm = require('wezterm')
 local config = wezterm.config_builder()
 
 config.window_decorations = "RESIZE"
-config.window_background_opacity = 0.7
+config.window_background_opacity = 0.9
 config.macos_window_background_blur = 30
 config.win32_system_backdrop = "Acrylic"
 
