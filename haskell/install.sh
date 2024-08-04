2aa6ec94700fbb8a8baef64d4180ec5b1d6e5098
Nelson Estevão <nelsonmestevao@proton.me>
2024-08-04 07:52:59 +0100

    [haskell]: improve install script

--- haskell/install.sh
+++ haskell/install.sh
@@ -3,10 +3,17 @@
 BASE_DIR=$(dirname "${BASH_SOURCE[0]:-$0}")
 cd "${BASE_DIR}/.." || exit 127
 
+# shellcheck source=../scripts/distro.sh
+. scripts/distro.sh
 # shellcheck source=../scripts/execs.sh
 . scripts/execs.sh
 
 function install_ghcup() {
+  if [[ -f "${HOME}/.ghcup/bin/ghcup" ]]; then
+    echo "GHCup is already installed."
+    return
+  fi
+
   curl --proto '=https' --tlsv1.2 -sSf https://get-ghcup.haskell.org | BOOTSTRAP_HASKELL_NONINTERACTIVE=1 sh
 }
 
@@ -22,12 +29,22 @@ function install_tools() {
   declare -a tools=(hlint hindent)
 
   for tool in "${tools[@]}"; do
-    cabal install --lib $tool
+    cabal install $tool
   done
+}
+
+function download_package_list() {
+  cabal update
 }
 
 symlink "$HOME/.dotfiles/haskell/ghci" "$HOME/.ghci"
 
+# sudo dnf install gmp-devel
+
 execute install_ghcup "Installing GHCup..."
+
+source haskell/haskell.init
+
+execute download_package_list "Download the package list for all remotes..."
 execute install_libraries "Installing Haskell libraries..."
 execute install_tools "Installing Haskell tools..."
