49bb84b7c4d786f2ae308a44ed2ee77a4158322d
Nelson Estevão <nelsonmestevao@proton.me>
2024-11-02 15:41:19 +0000

    [gnome]: update install setup

--- gnome/install.sh
+++ gnome/install.sh
@@ -1,6 +1,6 @@
 dconf write /org/gnome/desktop/input-sources/xkb-options "['caps:ctrl_modifier']"
 
-dconf write '/org/gnome/shell/extensions/pop-shell/toggle-floating' "['<Super><Shift>Space']"
+# dconf write '/org/gnome/shell/extensions/pop-shell/toggle-floating' "['<Super><Shift>Space']"
 dconf write '/org/gnome/shell/extensions/pop-shell/toggle-floating' "['<Meta><Shift>Space']"
 
 # gsettings get org.gnome.desktop.interface color-scheme
