e2fbf47781fe0f694420a76751058894c68c46bf
Nelson Estevão <nelsonmestevao@proton.me>
2025-01-04 21:49:43 +0000

    [nix]: add config for experimental features for flakes and nix-command

---
+++ nix/install.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+BASE_DIR=$(dirname "${BASH_SOURCE[0]:-$0}")
+cd "${BASE_DIR}/.." || exit 127
+
+# shellcheck source=../scripts/distro.sh
+. scripts/distro.sh
+# shellcheck source=../scripts/execs.sh
+. scripts/execs.sh
+# shellcheck source=../scripts/utils.sh
+. scripts/utils.sh
+
+ask_for_sudo
+
+mkdir -p ~/.config/nix
+symlink ~/.dotfiles/nix/nix.conf ~/.config/nix/nix.conf
