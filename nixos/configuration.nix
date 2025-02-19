2db674e9eba68eb333513a3967300ab8fa1f834c
Nelson Estevão <nelsonmestevao@proton.me>
2025-02-19 23:18:09 +0000

    [nixos]: remove warnings of old setup options

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -65,7 +65,7 @@
   services.printing.enable = true;
 
   # Enable sound with pipewire.
-  hardware.pulseaudio.enable = false;
+  services.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
