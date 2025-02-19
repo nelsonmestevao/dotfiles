008c858c1af86cbca40d6f48a6856144f9e4459a
Nelson Estevão <nelsonmestevao@proton.me>
2025-02-19 23:12:33 +0000

    [nixos]: add sc-im as a package for home-manager

--- nixos/home.nix
+++ nixos/home.nix
@@ -20,7 +20,7 @@
   home.packages = [
     # # Adds the 'hello' command to your environment. It prints a friendly
     # # "Hello, world!" when run.
-    pkgs.hello
+    pkgs.sc-im
 
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
