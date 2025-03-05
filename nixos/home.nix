2d18cdffd49106920cfb8d92c10961f83a3badf9
Nelson Estevão <nelsonmestevao@proton.me>
2025-03-05 21:41:10 +0000

    [nixos]: add development packages

--- nixos/home.nix
+++ nixos/home.nix
@@ -93,6 +93,14 @@ with lib.hm.gvariant;
     ruby_3_4
     nodejs_23
 
+    file
+    image_optim
+    # jpegoptim
+    # optipng
+    # pngquant
+    # nodePackages.svgo
+    # gifsicle
+
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
