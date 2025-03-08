dca682c6ef9f58b518afbeb4e7bc4e43d213f0e9
Nelson Estevão <nelsonmestevao@proton.me>
2025-03-08 19:04:30 +0000

    [jetbrains]: update ideavimrc config

--- jetbrains/install.sh
+++ jetbrains/install.sh
@@ -5,7 +5,11 @@
 BASE_DIR=$(dirname "${BASH_SOURCE[0]:-$0}")
 cd "${BASE_DIR}/.." || exit 127
 
+# shellcheck source=../scripts/distro.sh
+. scripts/distro.sh
 # shellcheck source=../scripts/execs.sh
 . scripts/execs.sh
+# shellcheck source=../scripts/utils.sh
+. scripts/utils.sh
 
 symlink "$HOME/.dotfiles/jetbrains/ideavimrc" "$HOME/.ideavimrc"
