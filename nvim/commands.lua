795ca9a2ecc331c00550018d418ae366ed5f3ee9
Nelson Estevão <nelsonmestevao@proton.me>
2025-03-26 14:11:54 +0000

    [vim]: add FormatEnable and FormatDisable to remove auto format

--- nvim/commands.lua
+++ nvim/commands.lua
@@ -19,6 +19,27 @@ vim.api.nvim_create_user_command("EN", function()
   changespell("en")
 end, {})
 
+-- ## Formatting
+--
+vim.api.nvim_create_user_command("FormatDisable", function(args)
+  if args.bang then
+    -- FormatDisable! will disable formatting just for this buffer
+    vim.b.disable_autoformat = true
+  else
+    vim.g.disable_autoformat = true
+  end
+end, {
+  desc = "Disable autoformat-on-save",
+  bang = true,
+})
+
+vim.api.nvim_create_user_command("FormatEnable", function()
+  vim.b.disable_autoformat = false
+  vim.g.disable_autoformat = false
+end, {
+  desc = "Re-enable autoformat-on-save",
+})
+
 -- ## Conveniences
 
 vim.api.nvim_create_autocmd("BufReadPost", {
