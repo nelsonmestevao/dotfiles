ca247a3b8caf77de9e60eb3d33bbd1870df637e0
Nelson Estevão <nelsonmestevao@proton.me>
2025-10-19 17:08:55 +0100

    [nixos]: remove unnecessary variable

--- nixos/home/lib/mkHomeModule.nix
+++ nixos/home/lib/mkHomeModule.nix
@@ -6,13 +6,10 @@ name: body:
   pkgs,
   ...
 }:
-let
-  cfg = config.dotfiles.programs.${name};
-in
 {
   options.dotfiles.programs.${name}.enable = lib.mkEnableOption "Enable ${name} configuration.";
 
-  config = lib.mkIf cfg.enable (body {
+  config = lib.mkIf config.dotfiles.programs.${name}.enable (body {
     inherit config lib pkgs;
   });
 }
