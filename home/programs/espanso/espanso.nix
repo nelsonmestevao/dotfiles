7c3ea33230fda16189e501980adaa74a836ffb0b
Nelson Estevão <nelsonmestevao@proton.me>
2025-10-26 12:28:12 +0000

    [espanso]: use mkSymlink instead of recursive options

--- home/programs/espanso/espanso.nix
+++ home/programs/espanso/espanso.nix
@@ -10,13 +10,6 @@
     espanso-wayland
   ];
 
-  xdg.configFile."espanso/config" = {
-    source = ./config;
-    recursive = true;
-  };
-
-  xdg.configFile."espanso/match" = {
-    source = ./match;
-    recursive = true;
-  };
+  xdg.configFile."espanso/config" = mkSymlink "config";
+  xdg.configFile."espanso/match" = mkSymlink "match";
 }
