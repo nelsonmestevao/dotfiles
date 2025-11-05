f4afdeee18ac7dfa07878fcd67fbfd39e6f8f8f8
Nelson Estevão <nelsonmestevao@proton.me>
2025-11-05 19:09:20 +0000

    refactor: create nelson user module

--- home/default.nix
+++ home/default.nix
@@ -8,6 +8,7 @@
 {
   imports = [
     ./packages.nix
+    ./users/${username}.nix
   ];
 
   home.username = username;
@@ -19,49 +20,6 @@
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
   programs.nh.enable = true;
-
-  # Shell
-  dotfiles.programs.zsh.enable = true;
-
-  # WM
-  dotfiles.programs.hyprland.enable = false;
-
-  # Desktop Environments
-  dotfiles.programs.gnome.enable = true;
-
-  # Programming Languages
-  dotfiles.programs.elixir.enable = true;
-  dotfiles.programs.erlang.enable = true;
-  dotfiles.programs.golang.enable = true;
-  dotfiles.programs.haskell.enable = true;
-  dotfiles.programs.nodejs.enable = true;
-  dotfiles.programs.python.enable = true;
-  dotfiles.programs.ruby.enable = true;
-
-  # Programs and Tools
-  dotfiles.programs.bat.enable = true;
-  dotfiles.programs.direnv.enable = true;
-  dotfiles.programs.espanso.enable = false;
-  dotfiles.programs.fzf.enable = true;
-  dotfiles.programs.ghostty.enable = true;
-  dotfiles.programs.git.enable = true;
-  dotfiles.programs.jetbrains.enable = true;
-  dotfiles.programs.jj.enable = true;
-  dotfiles.programs.latex.enable = true;
-  dotfiles.programs.mise.enable = true;
-  dotfiles.programs.mysql.enable = false;
-  dotfiles.programs.nvim.enable = true;
-  dotfiles.programs.rclone.enable = true;
-  dotfiles.programs.restic.enable = false;
-  dotfiles.programs.sqlite.enable = true;
-  dotfiles.programs.ssh.enable = true;
-  dotfiles.programs.vscode.enable = true;
-  dotfiles.programs.wezterm.enable = true;
-  dotfiles.programs.wine.enable = true;
-  dotfiles.programs.zed.enable = true;
-  dotfiles.programs.zoxide.enable = true;
-
-  dotfiles.packages.enable = true;
 
   # This value determines the Home Manager release that your configuration is
   # compatible with. This helps avoid breakage when a new Home Manager release
---
+++ home/users/nelson.nix
@@ -0,0 +1,50 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+{
+  # Shell
+  dotfiles.programs.zsh.enable = true;
+
+  # WM
+  dotfiles.programs.hyprland.enable = false;
+
+  # Desktop Environments
+  dotfiles.programs.gnome.enable = true;
+
+  # Programming Languages
+  dotfiles.programs.elixir.enable = true;
+  dotfiles.programs.erlang.enable = true;
+  dotfiles.programs.golang.enable = true;
+  dotfiles.programs.haskell.enable = true;
+  dotfiles.programs.nodejs.enable = true;
+  dotfiles.programs.python.enable = true;
+  dotfiles.programs.ruby.enable = true;
+
+  # Programs and Tools
+  dotfiles.programs.bat.enable = true;
+  dotfiles.programs.direnv.enable = true;
+  dotfiles.programs.espanso.enable = false;
+  dotfiles.programs.fzf.enable = true;
+  dotfiles.programs.ghostty.enable = true;
+  dotfiles.programs.git.enable = true;
+  dotfiles.programs.jetbrains.enable = true;
+  dotfiles.programs.jj.enable = true;
+  dotfiles.programs.latex.enable = true;
+  dotfiles.programs.mise.enable = true;
+  dotfiles.programs.mysql.enable = false;
+  dotfiles.programs.nvim.enable = true;
+  dotfiles.programs.rclone.enable = true;
+  dotfiles.programs.restic.enable = false;
+  dotfiles.programs.sqlite.enable = true;
+  dotfiles.programs.ssh.enable = true;
+  dotfiles.programs.vscode.enable = true;
+  dotfiles.programs.wezterm.enable = true;
+  dotfiles.programs.wine.enable = true;
+  dotfiles.programs.zed.enable = true;
+  dotfiles.programs.zoxide.enable = true;
+
+  dotfiles.packages.enable = true;
+}
