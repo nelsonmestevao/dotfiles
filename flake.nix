f820913ed740b319fbfaab62254d60aed3a6ef43
Nelson Estevão <nelsonmestevao@proton.me>
2026-06-01 15:18:20 +0100

    chore: upgrade to nixos-26.05

--- flake.nix
+++ flake.nix
@@ -1,11 +1,11 @@
 {
   description = "Nelson's Dotfiles";
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-25.11";
+      url = "github:nix-community/home-manager/release-26.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
