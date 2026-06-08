c16368804e21cf4ef6e34a7a897f64af88a19942
Nelson Estevão <nelsonmestevao@proton.me>
2026-06-08 11:58:15 +0100

    [gnome]: configure wallpapers change cycle

--- home/programs/gnome/extensions/wallpaper-slideshow/wallpaper-slideshow.nix
+++ home/programs/gnome/extensions/wallpaper-slideshow/wallpaper-slideshow.nix
@@ -8,8 +8,9 @@
 {
   # ── dconf Settings ───────────────────────────────────────────────────
   dconf.settings."org/gnome/shell/extensions/azwallpaper" = {
-    slideshow-directory = "";
-    slideshow-show-quick-settings-entry = false;
+    slideshow-directory = "/home/${username}/Pictures/Bing Wallpapers";
+    slideshow-pause-notifications = false;
+    slideshow-show-quick-settings-entry = true;
     bing-download-directory = "/home/${username}/Pictures/Bing Wallpapers";
     bing-wallpaper-download = true;
     bing-wallpaper-download-count = 8;
